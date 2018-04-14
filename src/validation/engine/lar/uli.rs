45ac49487f84048b8a2b3031c5432dac0437184e
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-04-14 03:31:27 -0400

    Stub for check digit

---
+++ src/validation/engine/lar/uli.rs
@@ -0,0 +1,17 @@
+pub fn check_digit() {}
+
+fn calculate_mod(i: i32) -> i32 {
+    i % 97
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    //#[test]
+    //test_fn_calculate_mod() {
+    //  assert_eq!(calculate_mod(100), 1)
+    //}
+
+}
