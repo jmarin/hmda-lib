63c0f7ad15ef5b26b5d06627eff08a2794a54d82
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-04-19 19:36:53 -0400

    Update test

--- src/validation/engine/lar/uli.rs
+++ src/validation/engine/lar/uli.rs
@@ -1,6 +1,8 @@
+//See https://www.consumerfinance.gov/eregulations/1003-C/2015-26607_20180101#1003-C-1
+
 pub fn check_digit() {}
 
-fn calculate_mod(i: i32) -> i32 {
+fn calculate_mod(i: u64) -> u64 {
     i % 97
 }
 
@@ -9,9 +11,9 @@ mod tests {
 
     use super::*;
 
-    //#[test]
-    //test_fn_calculate_mod() {
-    //  assert_eq!(calculate_mod(100), 1)
-    //}
+    #[test]
+    fn test_calculate_mod() {
+        assert_eq!(calculate_mod(100), 3);
+    }
 
 }
